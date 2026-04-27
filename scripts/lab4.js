09d68d6780673cd640174a109858978b62311435
Jaylen Cun <jscun@ucsd.edu>
2026-04-27 05:11:19 -0700

    finalized and fixed the bugs in the scripts folder.

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -28,11 +28,11 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    if(prices.length === 0) {
+    if(typeof discount !== 'number' || !Array.isArray(prices)) {
         return false;
     }
-
-    if(typeof discount !== 'number' || !Array.isArray(prices)) {
+    
+    if(prices.length === 0) {
         return false;
     }
 
@@ -40,7 +40,7 @@ function discountPrices(prices, discount) {
     const length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
